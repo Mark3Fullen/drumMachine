23ae8031abcc1f71abc3ef4ce0dfffc505fd7c6d
Mark <mark3fullen@gmail.com>
2022-01-16 12:18:50 -0500

    Revising into loop

--- src/App.js
+++ src/App.js
@@ -1,53 +1,27 @@
 import './App.css';
 
-const libOne = document.getElementById('Lib1')
-const libTwo = document.getElementById('Lib2')
-const libThree = document.getElementById('Lib3')
-const beatName = document.getElementById('beat-name')
-const padContainer = document.getElementById('pad-container')
+let bpm = 100
+let notes = 16
+let rows = 5
 
-function sound(src) {
-  this.sound = document.createElement('audio')
-  this.sound.src = src
-  this.sound.setAttribute('preload', 'auto')
-  this.sound.setAttribute('controls', 'none')
-  this.sound.classList.add('clip')
-  this.sound.style.display = 'none'
-  document.body.appendChild(this.sound)
-  this.play = function() {
-    this.sound.play()
-  }
-  this.stop = function() {
-    this.sound.pause()
-  }
+const calculateBPM = () => {
+  return Math.round( ( (44100/60) / (bpm/notes) ) / notes )
 }
 
 function App() {
   return (
     <div className="container">
-      <div id="drum-machine">
-        <div className="btn-group">
-          <div id="lib1">Library One</div>
-          <div id="lib2">Library Two</div>
-          <div id="lib3">Library Three</div> 
-        </div>
-        <div id="display">
-          <p id="beat-name">
-            Click a <strong>Pad</strong> or press a <strong>Key</strong>
-          </p>
-        </div>
-        <div id="pad-container">
-          <div className="drum-pad" id="Q"></div>
-          <div className="drum-pad" id="W"></div>
-          <div className="drum-pad" id="E"></div>
-          <div className="drum-pad" id="A"></div>
-          <div className="drum-pad" id="S"></div>
-          <div className="drum-pad" id="D"></div>
-          <div className="drum-pad" id="Z"></div>
-          <div className="drum-pad drum-pad-hover" id="X"></div>
-          <div className="drum-pad drum-pad-clicked" id="C"></div>
-        </div>
-      </div>
+      <header>
+        <button id="play-pause" data-playing="false">
+          <i className="fa fa-play"></i>
+        </button>
+      </header>
+      <div className="container player"></div>
+      <audio id="1"></audio>
+      <audio id="2"></audio>
+      <audio id="3"></audio>
+      <audio id="4"></audio>
+      <audio id="5"></audio>
     </div>
   );
 }
