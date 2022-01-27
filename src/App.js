a7649e8ce60e71a3afdc3cb080a9d9f9cb6a5317
Mark <mark3fullen@gmail.com>
2022-01-27 18:08:39 -0500

    Adding sounds and render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import loop1 from "./soundPack/Loop1.wav";
+import loop2 from "./soundPack/Loop2.wav";
+import loop3 from "./soundPack/Loop3.wav";
+import snare from "./soundPack/Snare.wav";
+import kick from "./soundPack/Kick.wav";
 
 class App extends React.Component {
   constructor(props) {
@@ -28,9 +33,9 @@ class App extends React.Component {
     this.handleSlide = this.handleSlide.bind(this);
 
     this.drumKey = {
-      Q: "Clap",
-      W: "",
-      E: "",
+      Q: "Loop 1",
+      W: "Loop 2",
+      E: "Loop 3",
       A: "",
       S: "",
       D: "",
@@ -45,7 +50,7 @@ class App extends React.Component {
     document.addEventListener("keyup", this.handleKeyUp);
   }
 
-  componentDidUnmount() {
+  componentWillUnmount() {
     document.addEventListener("keydown", this.handleKeyPress);
     document.addEventListener("keyup", this.handleKeyUp);
   }
@@ -124,7 +129,49 @@ class App extends React.Component {
   }
 
   render() {
-    
+    return (
+
+      <div className="App" id="drum-machine">
+
+        <div className="header">
+          <button type="button" className="power-btn" onClick={this.togglePower} style={this.state.power ? {backgroundColor: "#2ec4b6"} : {backgroundColor: "#ff3366"}}>
+            {this.state.power ? "ON" : "OFF"}
+          </button>
+          <div className="display" id="display">
+            <p>{this.state.input}</p>
+          </div>
+        </div>
+
+        <div className="drum-pad-container">
+          <div className={"drum-pad active-" + this.state.Q} onMouseDown={() => {this.handleMouseDown("Q")}} onMouseUp={this.handleMouseUp} id="dp-Q">
+            Q
+            <audio src={loop1} className="loop1" id="Q" preload="auto" volume={this.state.volume}/>
+          </div>
+          <div className={"drum-pad active-" + this.state.W} onMouseDown={() => {this.handleMouseDown("W")}} onMouseUp={this.handleMouseUp} id="dp-W">
+            W
+            <audio src={loop2} className="loop2" id="W" preload="auto" volume={this.state.volume}/>
+          </div>
+          <div className={"drum-pad active-" + this.state.E} onMouseDown={() => {this.handleMouseDown("E")}} onMouseUp={this.handleMouseUp} id="dp-E">
+            E
+            <audio src={loop3} className="loop3" id="E" preload="auto" volume={this.state.volume}/>
+          </div>
+          <div className={"drum-pad active-" + this.state.A} onMouseDown={() => {this.handleMouseDown("A")}} onMouseUp={this.handleMouseUp} id="dp-A">
+            A
+            <audio src={kick} className="kick" id="A" preload="auto" volume={this.state.volume}/>
+          </div>
+          <div className={"drum-pad active-" + this.state.S} onMouseDown={() => {this.handleMouseDown("S")}} onMouseUp={this.handleMouseUp} id="dp-S">
+            S
+            <audio src={snare} className="snare" id="S" preload="auto" volume={this.state.volume}/>
+          </div>
+        </div>
+
+        <div className="slider-container">
+          <label htmlFor="volume">Volume</label>
+          <input className="slider" type="range" id="start" name="volume" min="0" max="10" onChange={this.handleSlide} />
+        </div>
+
+      </div>
+    )
   }
 
 }
