206ccd840640f6265bc22b1da3e41cbefa2c4e6c
Mark <mark3fullen@gmail.com>
2022-01-27 15:55:40 -0500

    Updating and changing design...again

--- src/App.js
+++ src/App.js
@@ -1,53 +1,132 @@
-import React, {useState, useEffect} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const snareSound = "https://freewavesamples.com/files/Ensoniq-ESQ-1-Snare.wav"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const useAudio = url => {
-  const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
-
-  const toggle = () => setPlaying(!playing);
-
-  useEffect(() => {
-
-      playing ? audio.play() && console.log("Bruh") : audio.pause();
-
-    },
-    [playing]
-  );
-
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
+    this.state = {
+      power: true,
+      input: '',
+      volume: 1.0,
+      Q: false,
+      W: false,
+      E: false,
+      A: false,
+      S: false,
+      D: false,
+      Z: false,
+      X: false,
+      C: false
     };
-  }, []);
 
-  return [playing, toggle];
-};
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.togglePower = this.togglePower.bind(this);
+    this.handleSlide = this.handleSlide.bind(this);
 
-const Player = ({ url }) => {
-  const [playing, toggle] = useAudio(url);
+    this.drumKey = {
+      Q: "Clap",
+      W: "",
+      E: "",
+      A: "",
+      S: "",
+      D: "",
+      Z: "",
+      X: "",
+      C: ""
+    }
+  }
 
-  return (
-    <div>
-      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
-    </div>
-  );
-};  
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
 
-function App() {
-  return (
-    <div className="container">
-      <header>
-        DruMachine 1.0!
-      </header>
-      <div className="container-player">
-        {Player(snareSound)}
-      </div>
-    </div>
-  );
+  componentDidUnmount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  handleMouseDown(key) {
+    if (this.state.power) {
+      this.setState({
+        input: this.drumKey[key],
+        [key]: true
+      });
+      document.getElementById(key).volume = this.state.volume;
+      document.getElementById(key).pause();
+      document.getElementById(key).currentTime = 0;
+      document.getElementById(key).play();
+    } else {
+      this.setState({
+        input: "Power is off"
+      });
+    }
+  }
+
+  handleMouseUp(event) {
+    this.setState({
+      Q: false,
+      W: false,
+      E: false,
+      A: false,
+      S: false,
+      D: false,
+      Z: false,
+      X: false,
+      C: false
+    })
+  }
+
+  handleKeyPress(event) {
+    if (this.state.power) {
+      const keyTest = /[QWEASDZXC]/.test(event.key);
+      if (keyTest) {
+        this.setState({
+          input: this.drumKey[event.key],
+          [event.key]: true
+        });
+        document.getElementById(event.key).pause();
+        document.getElementById(event.key).currentTime = 0;
+        document.getElementById(event.key).play();
+      }
+    }
+  }
+
+  togglePower() {
+    this.setState({
+      power: !this.state.power,
+      input: this.state.power ? "Power is OFF" : "Power is ON"
+    });
+  }
+
+  handleSlide(event) {
+    this.setState({
+      volume: event.target.value / 10
+    })
+  }
+
+  handleKeyUp(event) {
+    this.setState({
+      Q: false,
+      W: false,
+      E: false,
+      A: false,
+      S: false,
+      D: false,
+      Z: false,
+      X: false,
+      C: false
+    });
+  }
+
+  render() {
+    
+  }
+
 }
 
 export default App;
